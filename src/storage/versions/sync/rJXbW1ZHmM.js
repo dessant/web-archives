34215bf8249f2bfc1bcfddb6e62c67d1b8993cdb
dessant <git@rmn.space>
2017-12-30 14:03:43 +0200

    fix: do not show page action on server error by default

---
+++ src/storage/versions/sync/rJXbW1ZHmM.js
@@ -0,0 +1,30 @@
+import browser from 'webextension-polyfill';
+
+const message = 'Set showPageAction to false';
+
+const revision = 'rJXbW1ZHmM';
+const downRevision = 'SkhmnNhMG';
+
+const storage = browser.storage.sync;
+
+async function upgrade() {
+  const changes = {};
+  changes.showPageAction = false;
+
+  changes.storageVersion = revision;
+  return storage.set(changes);
+}
+
+async function downgrade() {
+  const changes = {};
+
+  changes.storageVersion = downRevision;
+  return storage.set(changes);
+}
+
+module.exports = {
+  message,
+  revision,
+  upgrade,
+  downgrade
+};
