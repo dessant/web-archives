8417c78a4b36785c4e931454f0905de461f35296
dessant <git@armin.dev>
2026-02-15 09:30:20 +0200

    chore: update Chrome user agent

--- src/utils/data.js
+++ src/utils/data.js
@@ -150,10 +150,10 @@ const linkArchiveUrlRx = {
 };
 
 const chromeDesktopUA =
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36';
 
 const chromeMobileUA =
-  'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36';
+  'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Mobile Safari/537.36';
 
 const supportUrl = 'https://github.com/dessant/web-archives/issues';
 
